2b8e6c24237b66e5d6f9ec91c6b9e6824d81f83e
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2019-12-23 17:46:57 +0800

    add readwav c and h

--- readwav.c
+++ readwav.c
@@ -9,5 +9,21 @@ int argc;
 char* argv[];
 {
     //read the arguments
+    char * filename = "../wav/Nellie1.wav";
+    FILE * fr = fopen(filename,"r");
+
+    if ( fr == NULL){
+        printf("Fail to open file ");
+        return 0;
+    }
+    printf("open succeed.");
+    int c;
+    while((c = fgetc(fr)) != EOF){
+        
+    }
+
+    fclose(fr);
+
+
 
 }
